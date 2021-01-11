e042861d1dc1b20f66ced7d43c6b8485ebbde77b
Silvio Tomatis <silviot@gmail.com>
2021-01-11 12:12:43 +0100

    Refactor retrying in tests

--- eisbuk-admin/src/tests/slot.aggregation.test.js
+++ eisbuk-admin/src/tests/slot.aggregation.test.js
@@ -31,21 +31,10 @@ it("updates the slots summary on slot creation", async () => {
   });
   // Now check that the aggregate collection has been updated
   const aggregateSlotsQuery = org.collection("slotsByDay").doc("2021-01");
-  var aggregateSlot;
-  await retry(
-    // Try to fetch the aggregate slots for the day until
-    // we find the newly added one
-    async () => {
-      aggregateSlot = (await aggregateSlotsQuery.get()).data();
-      if (typeof aggregateSlot === "undefined") {
-        return Promise.reject(new Error("The aggregated slot was not found"));
-      }
-
-      return Promise.resolve();
-    },
-    10, // Try the above up to 10 times
-    () => 400 // pause 400 ms between tries
-  );
+  var aggregateSlot = await waitForRecord({
+    record: aggregateSlotsQuery,
+    num_keys: 1,
+  });
   expect(aggregateSlot["2021-01-30"].testSlot.type).toStrictEqual("ice");
 
   // Create another slot on the previous day
@@ -56,20 +45,30 @@ it("updates the slots summary on slot creation", async () => {
     durations: [60, 90, 120],
     category: "agonismo",
   });
-  await retry(
+  aggregateSlot = await waitForRecord({
+    record: aggregateSlotsQuery,
+    num_keys: 2,
+  });
+  expect(aggregateSlot["2021-01-29"].anotherSlot.type).toStrictEqual("ice");
+});
+
+async function waitForRecord({ record, num_keys }) {
+  // retry to get the given record until it contains the expected number of keys
+  var removeme = await retry(
     // Try to fetch the aggregate slots for the day until
     // we find the newly added one
     async () => {
-      aggregateSlot = (await aggregateSlotsQuery.get()).data();
-      if (Object.keys(aggregateSlot).length != 2) {
+      var aggregateSlot = (await record.get()).data();
+      if (Object.keys(aggregateSlot).length != num_keys) {
         return Promise.reject(
-          new Error("The aggregated slot does not include the new record")
+          new Error(`The aggregated slot with ${num_keys} keys was not found`)
         );
       }
-      return Promise.resolve();
+      return aggregateSlot;
     },
     10, // Try the above up to 10 times
     () => 400 // pause 400 ms between tries
   );
-  expect(aggregateSlot["2021-01-29"].anotherSlot.type).toStrictEqual("ice");
-});
+  console.log(removeme);
+  return removeme;
+}
