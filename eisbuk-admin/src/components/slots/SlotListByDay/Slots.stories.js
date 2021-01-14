c65fbd034455c276142dd7fd935896de85a4963e
Silvio Tomatis <silviot@gmail.com>
2021-01-14 14:26:09 +0100

    Refactor slot stories

--- eisbuk-admin/src/components/slots/SlotListByDay/Slots.stories.js
+++ eisbuk-admin/src/components/slots/SlotListByDay/Slots.stories.js
@@ -6,37 +6,36 @@ export default {
   component: SlotsDay,
 };
 
-const day = "2021-01-15";
-const slots_one = {
-  foo: {
-    category: "agonismo",
-    type: "ice",
-    date: { seconds: 1609513200 },
-    durations: [60],
-  },
-};
-const slots_many = {
-  ...slots_one,
-  bar: {
-    date: { seconds: 1609495200 },
-    category: "preagonismo",
-    type: "off-ice",
-    durations: [90, 120],
-  },
-  baz: {
-    date: { seconds: 1609516800 },
-    category: "agonismo",
-    type: "off-ice",
-    durations: [90, 120],
-  },
-};
-export const EmptyDay = () => (
-  <SlotsDay day={day} slots={{}} isCurrent={true} />
-);
+const Template = (args) => <SlotsDay {...args} />;
+export const ManySlots = Template.bind({});
 
-export const OneSlot = () => (
-  <SlotsDay day={day} slots={slots_one} isCurrent={true} />
-);
-export const ManySlots = () => (
-  <SlotsDay day={day} slots={slots_many} isCurrent={true} />
-);
+ManySlots.args = {
+  day: "2021-01-15",
+  isCurrent: true,
+  slots: {
+    foo: {
+      category: "agonismo",
+      type: "ice",
+      date: { seconds: 1609513200 },
+      durations: [60],
+    },
+    bar: {
+      date: { seconds: 1609495200 },
+      category: "preagonismo",
+      type: "off-ice",
+      durations: [90, 120],
+    },
+    baz: {
+      date: { seconds: 1609516800 },
+      category: "agonismo",
+      type: "off-ice",
+      durations: [60, 90, 120],
+    },
+  },
+};
+
+export const OneSlot = Template.bind({});
+OneSlot.args = { ...ManySlots.args, slots: { foo: ManySlots.args.slots.foo } };
+
+export const EmptyDay = Template.bind({});
+EmptyDay.args = { ...ManySlots.args, slots: {} };
