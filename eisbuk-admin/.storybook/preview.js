a8df7c0c821eeafb4cafd94396df4af3665198a0
Silvio Tomatis <silviot@gmail.com>
2021-01-14 11:32:40 +0100

    Add decorator to wrap all storybook stories inside a <Provide>

--- eisbuk-admin/.storybook/preview.js
+++ eisbuk-admin/.storybook/preview.js
@@ -1,3 +1,15 @@
+import React from "react";
+import { store } from "../src/store/store";
+import { Provider } from "react-redux";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <Story />
+    </Provider>
+  ),
+];
