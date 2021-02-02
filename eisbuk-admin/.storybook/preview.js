ca2eb9c4c9d78c872aa352e614f3bf9d09c3d4fe
Silvio Tomatis <silviot@gmail.com>
2021-02-02 14:21:30 +0100

    Set the state when passed a currentDate in a storybook story

--- eisbuk-admin/.storybook/preview.js
+++ eisbuk-admin/.storybook/preview.js
@@ -2,17 +2,23 @@ import React from "react";
 import { store } from "../src/store/store";
 import { StoryRouter } from "storybook-react-router";
 import { Provider } from "react-redux";
+import { changeCalendarDate } from "../src/store/actions/actions";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 };
 
 export const decorators = [
-  (Story) => (
-    <Provider store={store}>
-      <StoryRouter>
-        <Story />
-      </StoryRouter>
-    </Provider>
-  ),
+  (Story, context) => {
+    if (context.args.currentDate) {
+      store.dispatch(changeCalendarDate(context.args.currentDate));
+    }
+    return (
+      <Provider store={store}>
+        <StoryRouter>
+          <Story />
+        </StoryRouter>
+      </Provider>
+    );
+  },
 ];
