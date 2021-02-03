028cecda60cf10cdba88884da579da2c2bcd8828
Silvio Tomatis <silviot@gmail.com>
2021-02-03 16:31:24 +0100

    Use reselect to facilitate reuse and memoization of selectors

--- eisbuk-admin/src/store/selectors.js
+++ eisbuk-admin/src/store/selectors.js
@@ -1,39 +1,51 @@
+import { createSelector } from "reselect";
 import { fs2luxon } from "../utils/helpers";
 import * as _ from "lodash";
 
 export const calendarDaySelector = (state) => state.app.calendarDay;
 export const extractSlotDate = (slot) => slot.date.seconds;
 
-export const bookingDayInfoSelector = (dayStr) => (state) => {
+function getSafe(fn, defaultVal) {
+  // Try to execute the passed function. If it fails, return the default value
+  const def = defaultVal || {};
+  try {
+    return fn();
+  } catch (e) {
+    return def;
+  }
+}
+
+export const makeSlotsInfoDaySelector = (dayStr) => (state) => {
   const monthStr = dayStr.substr(0, 7);
-  const dayInfo =
-    ((state.firestore.data.slotsByDay &&
-      state.firestore.data.slotsByDay[monthStr]) ??
-      {})[dayStr] ?? {};
-  const bookingsInfo =
-    (state.firestore.data.bookingsByDay &&
-      state.firestore.data.bookingsByDay[monthStr]) ??
-    {};
-  const unsortedSlots = Object.keys(dayInfo).map((key) => dayInfo[key]);
-  const slots = _.sortBy(unsortedSlots, [extractSlotDate]);
-  const allUsers = state.firestore.data.customers;
-  return slots.map((slot) => {
-    const users = [];
-    Object.keys(bookingsInfo[slot.id] ?? {}).map((key) =>
-      users.push({
-        name: allUsers[key].name,
-        surname: allUsers[key].surname,
-        secret_key: allUsers[key].secret_key,
-        id: allUsers[key].id,
-      })
-    );
-    const info = {
-      time: fs2luxon(slot.date).toFormat("HH:mm"),
-      category: slot.category,
-      type: slot.type,
-      id: slot.id,
-      users: users,
-    };
-    return info;
-  });
+  return getSafe(() => state.firestore.data.slotsByDay[monthStr][dayStr]);
 };
+export const makeBookingsInfoSelector = (dayStr) => (state) =>
+  getSafe(() => state.firestore.data.bookingsByDay[dayStr.substr(0, 7)]);
+
+const allUsersSelector = (state) => state.firestore.data.customers;
+
+export const bookingDayInfoSelector = (dayStr) =>
+  createSelector(
+    makeSlotsInfoDaySelector(dayStr),
+    makeBookingsInfoSelector(dayStr),
+    allUsersSelector,
+    (slotsInfo, bookingsInfo, allUsers) => {
+      const unsortedSlots = Object.keys(slotsInfo).map((key) => slotsInfo[key]);
+      const slots = _.sortBy(unsortedSlots, [extractSlotDate]);
+      return slots.map((slot) => {
+        const users = Object.keys(bookingsInfo[slot.id] ?? {}).map((key) => ({
+          name: allUsers[key].name,
+          surname: allUsers[key].surname,
+          secret_key: allUsers[key].secret_key,
+          id: allUsers[key].id,
+        }));
+        return {
+          time: fs2luxon(slot.date).toFormat("HH:mm"),
+          category: slot.category,
+          type: slot.type,
+          id: slot.id,
+          users: users,
+        };
+      });
+    }
+  );
