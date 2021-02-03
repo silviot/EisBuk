1b2ce151db1bed6e546b72956f578ed41ce59008
Silvio Tomatis <silviot@gmail.com>
2021-02-03 12:27:06 +0100

    Use more all colors (all hues and five variants)

--- eisbuk-admin/src/components/users/coloredAvatar.js
+++ eisbuk-admin/src/components/users/coloredAvatar.js
@@ -2,45 +2,38 @@ import React from "react";
 
 import { Avatar } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import {
-  blue,
-  red,
-  yellow,
-  green,
-  orange,
-  purple,
-} from "@material-ui/core/colors";
+import * as colors from "@material-ui/core/colors";
 
 import { getInitials } from "../../utils/helpers";
 
-const useStyles = makeStyles((theme) => ({
-  blue: {
-    backgroundColor: blue[900],
-    color: theme.palette.getContrastText(blue[900]),
-  },
-  red: {
-    backgroundColor: red[900],
-    color: theme.palette.getContrastText(red[900]),
-  },
-  yellow: {
-    backgroundColor: yellow[900],
-    color: theme.palette.getContrastText(yellow[900]),
-  },
-  green: {
-    backgroundColor: green[900],
-    color: theme.palette.getContrastText(green[900]),
-  },
-  orange: {
-    backgroundColor: orange[900],
-    color: theme.palette.getContrastText(orange[900]),
-  },
-  purple: {
-    backgroundColor: purple[900],
-    color: theme.palette.getContrastText(purple[900]),
-  },
-}));
+// For all available colors make a CSS class
+const colorsDef = {};
+// eslint-disable-next-line array-callback-return
+Object.keys(colors).map((color) => {
+  if (color !== "common") {
+    colorsDef[color] = { backgroundColor: colors[color][900] };
+    colorsDef[color + "500"] = { backgroundColor: colors[color][500] };
+    colorsDef[color + "200"] = { backgroundColor: colors[color][200] };
+    colorsDef[color + "A100"] = { backgroundColor: colors[color]["A100"] };
+    colorsDef[color + "A200"] = { backgroundColor: colors[color]["A200"] };
+  }
+});
 
-const avatarColors = ["blue", "red", "yellow", "green", "orange", "purple"];
+// Make styles using the contrast text as text color
+const useStyles = makeStyles((theme) => {
+  const res = {};
+  Object.keys(colorsDef).map(
+    (color) =>
+      (res[color] = {
+        ...colorsDef[color],
+        color: theme.palette.getContrastText(colorsDef[color].backgroundColor),
+      })
+  );
+  return res;
+});
+
+const avatarColors = Object.keys(colorsDef);
+// Get the available classes we can use to get different colors
 
 export const ColoredAvatar = ({ name, surname }) => {
   let classes = useStyles();
