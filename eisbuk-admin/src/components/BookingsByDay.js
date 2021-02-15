48c67e5ac4d746838b075afa615ca82198124500
beppe <info@xfloor.it>
2021-02-15 12:25:19 +0100

    BookingsByDay UI improvements

--- eisbuk-admin/src/components/BookingsByDay.js
+++ eisbuk-admin/src/components/BookingsByDay.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {
   Button,
+  Container,
   List,
   ListItem,
   ListItemAvatar,
@@ -15,75 +16,82 @@ const BookingsByDay = ({ bookingDayInfo, markAbsentee }) => {
   const classes = useStyles();
   const [localAbsentees, setLocalAbsentees] = useState({});
   return (
-    <List className={classes.root}>
-      {bookingDayInfo.map((slot) => {
-        return (
-          <React.Fragment key={slot.id}>
-            <ListItem className={classes.listHeader}>
-              <ListItemText
-                primary={slot.time}
-                secondary={`${slot.category} ${slot.type}`}
-              />
-            </ListItem>
-            {slot.users.map((user) => {
-              var isAbsent = (slot.absentees || {})[user.id] ? true : false;
-              const hasLocalChange =
-                typeof (
-                  localAbsentees[slot.id] && localAbsentees[slot.id][user.id]
-                ) !== "undefined" &&
-                localAbsentees[slot.id][user.id] !== isAbsent;
-              if (hasLocalChange) {
-                isAbsent = !isAbsent;
-              }
-              const toggleAbsent = () => {
-                setLocalAbsentees((state) => ({
-                  ...state,
-                  [slot.id]: { ...state[slot.id], [user.id]: !isAbsent },
-                }));
-                markAbsentee({ slot, user, isAbsent: !isAbsent });
-              };
-              const absenteeButtons = markAbsentee ? (
-                <Button
-                  variant="contained"
-                  size="small"
-                  color={isAbsent ? "primary" : "secondary"}
-                  onClick={toggleAbsent}
-                  disabled={hasLocalChange}
-                >
-                  {isAbsent ? "👎" : "👍"}
-                </Button>
-              ) : null;
-              const listItemClass = isAbsent ? classes.absent : "";
-              const userName =
-                `${user.name} ${user.surname}` + (isAbsent ? " (assente)" : "");
-              return (
-                <ListItem
-                  key={`${slot.id}-${user.id}`}
-                  className={listItemClass}
-                >
-                  <ListItemAvatar>
-                    <ColoredAvatar {...user} />
-                  </ListItemAvatar>
-                  <ListItemText primary={userName} />
-                  <ListItemSecondaryAction>
-                    {absenteeButtons}
-                  </ListItemSecondaryAction>
-                </ListItem>
-              );
-            })}
-          </React.Fragment>
-        );
-      })}
-    </List>
+    <Container maxWidth="sm">
+      <List className={classes.root}>
+        {bookingDayInfo.map((slot) => {
+          return (
+            <div key={slot.id} className={classes.slotWrapper}>
+              <ListItem className={classes.listHeader}>
+                <ListItemText
+                  primary={slot.time}
+                  secondary={`${slot.category} ${slot.type}`}
+                />
+              </ListItem>
+              {slot.users.map((user) => {
+                var isAbsent = (slot.absentees || {})[user.id] ? true : false;
+                const hasLocalChange =
+                  typeof (
+                    localAbsentees[slot.id] && localAbsentees[slot.id][user.id]
+                  ) !== "undefined" &&
+                  localAbsentees[slot.id][user.id] !== isAbsent;
+                if (hasLocalChange) {
+                  isAbsent = !isAbsent;
+                }
+                const toggleAbsent = () => {
+                  setLocalAbsentees((state) => ({
+                    ...state,
+                    [slot.id]: { ...state[slot.id], [user.id]: !isAbsent },
+                  }));
+                  markAbsentee({ slot, user, isAbsent: !isAbsent });
+                };
+                const absenteeButtons = markAbsentee ? (
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color={isAbsent ? "primary" : "secondary"}
+                    onClick={toggleAbsent}
+                    disabled={hasLocalChange}
+                  >
+                    {isAbsent ? "👎" : "👍"}
+                  </Button>
+                ) : null;
+                const listItemClass = isAbsent ? classes.absent : "";
+                const userName =
+                  `${user.name} ${user.surname}` +
+                  (isAbsent ? " (assente)" : "");
+                return (
+                  <ListItem
+                    key={`${slot.id}-${user.id}`}
+                    className={listItemClass}
+                  >
+                    <ListItemAvatar>
+                      <ColoredAvatar {...user} />
+                    </ListItemAvatar>
+                    <ListItemText primary={userName} />
+                    <ListItemSecondaryAction>
+                      {absenteeButtons}
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                );
+              })}
+            </div>
+          );
+        })}
+      </List>
+    </Container>
   );
 };
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.secondary.light,
-  },
+  root: {},
   listHeader: {
     backgroundColor: theme.palette.primary.light,
+  },
+  slotWrapper: {
+    marginBottom: theme.spacing(1.5),
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: theme.palette.primary.main,
   },
   absent: {
     backgroundColor: theme.palette.absent || grey[500],
