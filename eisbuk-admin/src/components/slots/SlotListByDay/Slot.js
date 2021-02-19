ff54bdb40299ae3a147eea61787ea0293d806217
Silvio Tomatis <silviot@gmail.com>
2021-02-20 00:02:21 +0100

    Fix missing keys

--- eisbuk-admin/src/components/slots/SlotListByDay/Slot.js
+++ eisbuk-admin/src/components/slots/SlotListByDay/Slot.js
@@ -51,11 +51,17 @@ export default ({
       {!deleted && (
         <Card className={classes.root} raised={isSubscribed}>
           <CardContent>
-            <Typography display="inline" variant="h5" component="h2">
+            <Typography
+              display="inline"
+              variant="h5"
+              component="h2"
+              key="start"
+            >
               {date.toISOTime().substring(0, 5)}
             </Typography>
             {isSubscribed && (
               <Typography
+                key="end"
                 display="inline"
                 variant="h6"
                 component="h3"
@@ -76,7 +82,11 @@ export default ({
               !auth.isEmpty &&
               auth.isLoaded &&
               data.categories.map((category) => (
-                <Typography className={classes.category} color="textSecondary">
+                <Typography
+                  className={classes.category}
+                  color="textSecondary"
+                  key="category"
+                >
                   {category}
                 </Typography>
               ))}
