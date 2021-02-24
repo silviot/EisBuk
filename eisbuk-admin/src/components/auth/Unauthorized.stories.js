275b09be48e4c481e0510c624c079f629640f469
Silvio Tomatis <silviot@gmail.com>
2021-02-24 18:01:52 +0100

    Add storybook entry for unauthorized page

---
+++ eisbuk-admin/src/components/auth/Unauthorized.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Unauthorized from "./Unauthorized";
+
+export default {
+  title: "Unauthorized page",
+  component: Unauthorized,
+};
+
+export const Page = () => <Unauthorized />;
