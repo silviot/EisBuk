d748516d12dee881dcc170c32012ae96885d1d2f
Silvio Tomatis <silviot@gmail.com>
2021-06-28 17:20:49 +0200

    Run npx sb init

--- eisbuk-admin/.storybook/main.js
+++ eisbuk-admin/.storybook/main.js
@@ -4,40 +4,5 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "storybook-addon-material-ui",
   ],
-  babel: async (options) => ({
-    ...options,
-    plugins: [
-      ...options.plugins,
-      [
-        "babel-plugin-import",
-        {
-          libraryName: "@material-ui/core",
-          // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-          libraryDirectory: "esm",
-          camel2DashComponentName: false,
-        },
-        "core",
-      ],
-      [
-        "babel-plugin-import",
-        {
-          libraryName: "@material-ui/icons",
-          // Use "'libraryDirectory': ''," if your bundler does not support ES modules
-          libraryDirectory: "esm",
-          camel2DashComponentName: false,
-        },
-        "icons",
-      ],
-      [
-        "@babel/transform-runtime",
-        {
-          helpers: false,
-          regenerator: true,
-        },
-      ],
-    ],
-    presets: [...options.presets, "@babel/preset-env", "@babel/preset-react"],
-  }),
 };
